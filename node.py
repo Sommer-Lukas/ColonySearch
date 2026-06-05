d719e7eeeae7454cc9bd6b5a9f2de2b80d1d1981
tadicni96034 <tadicni96034@informatik.fh-nuernberg.de>
2026-06-05 16:06:24 +0200

    Added depth parameter and req_id to routing.
    Depth is just how far the request should be propagated into the graph,
    exactly like TTL in IP packets.
    
    req_id is generated for every new request and propagated along with it,
    each node has a cache of recent req_ids and makes sure that if one that
    it knows comes along again, it won't service that request, in order to
    save effort.
    
    The depth parameter by itself would prevent infinite loops in the
    request routing, but potentially the user would want very large depth
    parameters if we are talking about a global scale network, which would
    mean that loops would take a while to terminate and waste resources
    along the way.
    The req_id_cache terminates loops instantly, provided it still knows
    about the last time it saw the request in question.

--- node.py
+++ node.py
@@ -4,6 +4,7 @@ import requests
 import json
 from copy import deepcopy
 from time import sleep
+import uuid
 
 import search
 temp_search = search.search
@@ -66,61 +67,114 @@ def print_search_results(search_results: list):
                         # Maybe this has to do with my fucking git bash or something
 
 
+# NOTES ABOUT ROUTING:
+# Two parameters: depth and req_id
+# Depth is set by the user when making the request, determines how deep the search
+# is propagated into the network.
+# req_id is generated when the search is first requested and then forwarded to each node that
+# is indirectly queried to fulfill the request. If Node A knows Node B and Node B also knows Node A,
+# or any number of more complicated loops in the routing graph, then the req_id helps to avoid
+# useless querying. Each node has a req_id_cache and if it sees multiple requests come in with the same req_id,
+# it will just ignore the second one (or return nothing, or something, idk yet, TODO)
+# in order to save time.
+
+
+def gen_req_id():
+    result = uuid.uuid4()
+    print(f"DEBUG: gen_req_id called. result: {result}")
+    return result
+
+
 class Node:
-    def __init__(self, db_dir: str, neighbors: list = []):
+    def __init__(self, db_dir: str, neighbors: list, req_id_cache_max_length: int):
         self.local = True
         self.db_dir = db_dir
         self.remote_endpoint = None
         self.neighbors = neighbors
         self.rep = 0
 
-    def search(self, query: str):
-        if (self.local): return self._local_search(query)
-        else:            return self._remote_search(query)
+        # cache of recent request IDs. Used for avoiding looping in the request routing.
+        # The depth parameter is the other mechanism that avoids this, but the cache makes it
+        # better.
+        self.req_id_cache = []
+        self.req_id_cache_max_length = req_id_cache_max_length
 
-    def _local_search(self, query: str):
+    def _req_id_cache_new_entry(self, new_req_id):
+        # remove existing cache entry if it exists
+        for i, req_id in zip(range(len(self.req_id_cache)), self.req_id_cache):
+            if req_id == new_req_id:
+                self.req_id_cache.remove(i)
+
+        # place new cache entry at input end of FIFO
+        self.req_id_cache.append(new_req_id)
+
+        # remove oldest cache entry if cache is too large now
+        if len(self.req_id_cache) > self.req_id_cache_max_length:
+            self.req_id_cache.remove(0)
+
+    def _req_id_cache_query(self, query_req_id):
+        return query_req_id in self.req_id_cache
+
+    def search(self, query: str, remaining_depth: int, req_id):
+        if req_id == None:
+            req_id = gen_req_id()
+        else:
+            if self._req_id_cache_query(req_id) == True:
+                return [] # do as little as possible to conserve effort an simply return empty result list
+
+        self._req_id_cache_new_entry(req_id)
+
+        if (self.local): return self._local_search(query, remaining_depth, req_id)
+        else:            return self._remote_search(query, remaining_depth, req_id)
+
+    def _local_search(self, query: str, remaining_depth: int, req_id):
         query_vector = _encode_query(query)
         results = [res.to_dict() for res in temp_search(self.db_dir, query, query_vector)]
         for r in results:
             r.setdefault("node_id", self.db_dir)
         print_search_results(results)
 
-        councilor_nodes = self.get_neighbors(10)
+        if remaining_depth > 0:
+            remaining_depth -= 1;
 
-        councilor_nodes_scores = []
+            councilor_nodes = self.get_neighbors(10)
 
-        for node in councilor_nodes:
-            search_results = node.search(query)
-            print_search_results(search_results)
-            node_score = calc_node_score_from_search_results(search_results)
-            councilor_nodes_scores.append(node_score)
+            councilor_nodes_scores = []
 
-            for item in search_results:
-                item.setdefault("node_id", node.db_dir)
-                # TODO: make sure changing item changes search_results.
-                # TODO: think harder about whether we should use the old or new rep, maybe using the new rep has some advantage. It would effectively give a little more
-                # weight to the score of the results themselves. But I suppose a better way to do this would be to have a multiplier/slider for how much to consider
-                # the nodes rep vs how much to consider the quality of the results. The quality of the results can never 100% capture their actual quality,
-                # so historical data in the form of the running node reps is super useful because it completes the picture, at least to a certain degree.
-                # But how much weight to give one vs the other?
-                # Idk.
-                item["score"] += node.rep # add old node rep to its results. The calculated node score shouldn't be considered for the search results it was calculated from
+            for node in councilor_nodes:
+                search_results = node.search(query, remaining_depth, req_id)
+                print_search_results(search_results)
+                node_score = calc_node_score_from_search_results(search_results)
+                councilor_nodes_scores.append(node_score)
 
-            results += search_results
+                for item in search_results:
+                    item.setdefault("node_id", node.db_dir)
+                    # TODO: make sure changing item changes search_results.
+                    # TODO: think harder about whether we should use the old or new rep, maybe using the new rep has some advantage. It would effectively give a little more
+                    # weight to the score of the results themselves. But I suppose a better way to do this would be to have a multiplier/slider for how much to consider
+                    # the nodes rep vs how much to consider the quality of the results. The quality of the results can never 100% capture their actual quality,
+                    # so historical data in the form of the running node reps is super useful because it completes the picture, at least to a certain degree.
+                    # But how much weight to give one vs the other?
+                    # Idk.
+                    item["score"] += node.rep # add old node rep to its results. The calculated node score shouldn't be considered for the search results it was calculated from
 
-        for node, score in zip(councilor_nodes, councilor_nodes_scores):
-            node.rep += score
+                results += search_results
+
+            for node, score in zip(councilor_nodes, councilor_nodes_scores):
+                node.rep += score
 
         print('\n\n\n\nbetter together, no matter the weather, now, ladies and gentlemen, welcome all the search results:\n\n\n\n')
         print_search_results(results)
 
         return sort_search_results(results)[:10]
 
-    def _remote_search(self, query: str):
+    def _remote_search(self, query: str, remaining_depth: int, req_id):
         response = requests.get("http://{}:{}/api/search"
                                 .format(self.remote_endpoint[0], self.remote_endpoint[1]),
         {
-            "query": query
+            "query": query,
+            "remaining_depth": remaining_depth,
+            "req_id": req_id
         });
         return response.json()
 
@@ -129,16 +183,16 @@ class Node:
 
 
 myself = Node("data/dbs/Node_1", [
-    Node("data/dbs/Node_2", []),
-    Node("data/dbs/Node_3", []),
-    Node("data/dbs/Node_4", []),
-    Node("data/dbs/Node_5", []),
-    Node("data/dbs/Node_6", []),
-    Node("data/dbs/Node_7", []),
-    Node("data/dbs/Node_8", []),
-    Node("data/dbs/Node_9", []),
-    Node("data/dbs/Node_10", []),
-])
+    Node("data/dbs/Node_2",  [], 8),
+    Node("data/dbs/Node_3",  [], 8),
+    Node("data/dbs/Node_4",  [], 8),
+    Node("data/dbs/Node_5",  [], 8),
+    Node("data/dbs/Node_6",  [], 8),
+    Node("data/dbs/Node_7",  [], 8),
+    Node("data/dbs/Node_8",  [], 8),
+    Node("data/dbs/Node_9",  [], 8),
+    Node("data/dbs/Node_10", [], 8),
+], 8)
 
 
 
@@ -179,7 +233,9 @@ def search_page():
     if not query:
         return render_template("results.html", query="", results=[], error=None)
     try:
-        raw = myself.search(query)
+        # TODO: get depth from configurable value on the search panel in the frontend
+        depth = 3
+        raw = myself.search(query, depth, None) # req_id arg is always None here because this is brand new request
         results = _results_for_template(raw, node_id=myself.db_dir)
         return render_template("results.html", query=query, results=results, error=None)
     except Exception as exc:
@@ -187,8 +243,10 @@ def search_page():
 
 @app.route("/api/search", methods=["GET"])
 def api_search():
-    query = request.args.get("query")
-    return jsonify(myself.search(query))
+    query  = request.args.get("query")
+    depth  = request.args.get("depth")
+    req_id = request.args.get("req_id")
+    return jsonify(myself.search(query, depth, req_id))
 
 @app.route("/api/rep", methods=["GET"])
 def rep():
